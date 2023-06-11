56d400b2ca2239d352f7d9451646285791757b60
Sn_pierre <akrepierre123@gmail.com>
2023-06-11 03:13:49 +0200

    fin

--- equipe-de-redaction.php
+++ equipe-de-redaction.php
@@ -26,50 +26,51 @@ $listeAuteurs = $listeAuteursCommande->fetchAll();
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/header.css">
     <link rel="stylesheet" href="ressources/css/ne-pas-modifier/accueil.css">
     <link rel="icon" href="ressources/images/favicon-GEC_400x400px.png" type="image/png">
-
+    
     <link rel="stylesheet" href="ressources/css/global.css">
     <link rel="stylesheet" href="ressources/css/equipe-de-redaction.css">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <style>
-    /* Styles Tailwindcss*/
-    .liste-videos {
-      display: flex;
-      flex-direction : row;
-      justify-content: center;
-      gap: 20px;
-      padding: 145px;
-      list-style: none;
-      flex-wrap: wrap;
-      margin: 120px;
-    }
-    
-    .video-conteneur {
-    flex: 1 0 200px;
-    max-width: 200px;
-    text-align: center;
-    transition: transform 0.5s ease;
-    }
-
-    .video-conteneur:hover {
-    transform: scale(1.2);
-    }
-
-    
-    .avatar {
-      width: 100%;
-      height: auto;
-      border-radius: 124px;
-    }
-    
-    .titre {
-        font-family: Arial, sans-serif; 
-        font-size: 1.25rem; 
-        line-height: 1.5; 
-        margin-bottom: 20px; 
-        text-align: center; 
-        color: rgb(64, 9, 116);;
-    }
-  </style>
+        /* Styles Tailwindcss*/
+        .liste-videos {
+            display: flex;
+            flex-direction : row;
+            justify-content: center;
+            gap: 20px;
+            padding: 145px;
+            list-style: none;
+            flex-wrap: wrap;
+            margin: 120px;
+        }
+        
+        .video-conteneur {
+            flex: 1 0 200px;
+            max-width: 200px;
+            text-align: center;
+            transition: transform 0.5s ease;
+        }
+        
+        .video-conteneur:hover {
+            transform: scale(1.2);
+            opacity: 75%;
+        }
+        
+        
+        .avatar {
+            width: 100%;
+            height: auto;
+            border-radius: 124px;
+        }
+        
+        .prenom-nom {
+            font-family: Arial, sans-serif; 
+            font-size: 1.25rem; 
+            line-height: 1.5; 
+            margin-bottom: 20px; 
+            text-align: center; 
+            color: rgb(64, 9, 116);;
+        }
+        </style>
 
 </head>
 
@@ -86,7 +87,7 @@ $listeAuteurs = $listeAuteursCommande->fetchAll();
                 <?php foreach ($listeAuteurs as $auteur) { ?>
                     <a class="video-conteneur" href="https://twitter.com/UniversiteCergy">
                         <img class="avatar" src="<?php echo $auteur["lien_avatar"]?>"url="<?php echo $auteur["lien_twitter"]?>">
-                        <h2 class="titre"><?php echo $auteur["prenom"]?> <?php echo $auteur["nom"]?></h2>
+                        <h2 class="prenom-nom"><?php echo $auteur["prenom"]?> <?php echo $auteur["nom"]?></h2>
                 <?php } ?>
             </ul>
         </main>
